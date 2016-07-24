e94d4d1477d25b985d7b9608af8e3d4e85bdd10e
gercorr <gercorr@gmail.com>
2016-07-24 13:07:15 +0100

    Fixed fatal issue with timeout. Need to fix issue that requires refresh

--- src/main/java/com/geoshare/repositories/NoteRepository.java
+++ src/main/java/com/geoshare/repositories/NoteRepository.java
@@ -16,55 +16,28 @@ import com.geoshare.entities.Note;
 @Service
 public class NoteRepository implements INoteRepository {
 	
-	private EntityManager entityManager;
 	private EntityManagerFactory emfactory;
 
 	public NoteRepository()
 	{
 		emfactory = Persistence.createEntityManagerFactory( "Hibernate_JPA" );
-	    entityManager =  emfactory.createEntityManager();
 	}
 	
-	  public List<Note> getAllNotes() {		  		  
+	public List<Note> getAllNotes() {		  		 
+		EntityManager entityManager =  emfactory.createEntityManager();
+		Query query = entityManager.createQuery( "Select e from Note e ");			      
+		List<Note> list=(List<Note>)query.getResultList( );		
+		entityManager.close();
+		return list;  
+	}
 	  
-		  int count = 0;
-		  int maxTries = 2;
-		  while(true) {
-		      try {
-			      Query query = entityManager.createQuery( "Select e from Note e ");			      
-			      List<Note> list=(List<Note>)query.getResultList( );		   	     
-			      return list;
-	      } catch (Exception e) {
-	          // handle exception
-	          if (++count == maxTries) throw e;
-	
-	          	emfactory = Persistence.createEntityManagerFactory( "Hibernate_JPA" );
-			    entityManager =  emfactory.createEntityManager();
-		      }
-		  }  
-			  
-	
-	  }
-	  
-	  public void SaveNote(Note note)
-	  {
-		  int count = 0;
-		  int maxTries = 2;
-		  while(true) {
-		      try {
-		    	  entityManager.getTransaction( ).begin( );			  
-		    	  entityManager.persist( note );
-		    	  entityManager.getTransaction( ).commit( );
-		    	  return;
-		      } catch (Exception e) {
-		          // handle exception
-		          if (++count == maxTries) throw e;
-		
-		          	emfactory = Persistence.createEntityManagerFactory( "Hibernate_JPA" );
-				    entityManager =  emfactory.createEntityManager();
-			      }
-			  }  
-			
-	  }
+  public void SaveNote(Note note)
+  {
+		EntityManager entityManager =  emfactory.createEntityManager();
+		entityManager.getTransaction( ).begin( );			  
+		entityManager.persist( note );
+		entityManager.getTransaction( ).commit( );	
+	  	entityManager.close();		
+  }
 	  
 }
